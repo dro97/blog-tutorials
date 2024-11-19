675ef6bca1914d37121bd2c8f013c8eb74642e21
dro97 <diegoro.or@outlook.com>
2024-11-19 11:20:09 -0400

    Revert "se suben cambios de mongo"
    This reverts commit 60277ec93ad329bba1fdbb8b5d917b3757f5ba98.

--- springboot-jwt-auth/src/main/java/com/tericcabrel/authapi/entities/User.java
+++ springboot-jwt-auth/src/main/java/com/tericcabrel/authapi/entities/User.java
@@ -1,7 +1,9 @@
 
 package com.tericcabrel.authapi.entities;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,175 +11,125 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-
+@Table(name = "users")
+@Entity
 public class User implements UserDetails {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
 
+    @Column(nullable = false)
     private String fullName;
 
-    private String nick;
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
     private Date createdAt;
 
+    @UpdateTimestamp
+    @Column(name = "updated_at")
     private Date updatedAt;
 
-	public String getId() {
-		return id;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getFullName() {
-		return fullName;
-	}
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	public String getNick() {
-		return nick;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public Date getCreatedAt() {
-		return createdAt;
-	}
+    public User setId(Integer id) {
+        this.id = id;
+        return this;
+    }
 
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
+    public String getFullName() {
+        return fullName;
+    }
 
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
+    public User setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
 
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of();
-	}
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
 
-	@Override
-	public String getUsername() {
-		return nick;
-	}
-    
-    
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
 
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return List.of();
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return data;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public User setId(String id) {
-//        this.id = id;
-//        return this;
-//    }
-//
-//    public String getFullName() {
-//        return fullName;
-//    }
-//
-//    public User setFullName(String fullName) {
-//        this.fullName = fullName;
-//        return this;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public User setEmail(String email) {
-//        this.email = email;
-//        return this;
-//    }
-//
-//    public User setPassword(String password) {
-//        this.password = password;
-//        return this;
-//    }
-//
-//    public Date getCreatedAt() {
-//        return createdAt;
-//    }
-//
-//    public User setCreatedAt(Date createdAt) {
-//        this.createdAt = createdAt;
-//        return this;
-//    }
-//
-//    public Date getUpdatedAt() {
-//        return updatedAt;
-//    }
-//
-//    public User setUpdatedAt(Date updatedAt) {
-//        this.updatedAt = updatedAt;
-//        return this;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "id=" + id +
-//                ", fullName='" + fullName + '\'' +
-//                ", email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                ", createdAt=" + createdAt +
-//                ", updatedAt=" + updatedAt +
-//                '}';
-//    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public User setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public User setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
 
--- springboot-jwt-auth/src/main/java/com/tericcabrel/authapi/repositories/UserRepository.java
+++ springboot-jwt-auth/src/main/java/com/tericcabrel/authapi/repositories/UserRepository.java
@@ -1,13 +1,12 @@
 package com.tericcabrel.authapi.repositories;
 
 import com.tericcabrel.authapi.entities.User;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
